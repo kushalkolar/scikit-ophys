ba63a87290496d5bb96bf9aef51e182744f25bc9
ruoyushang <r.y.shang@gmail.com>
2025-08-08 14:54:34 -0400

    add background component

--- skophys/datasets/_synthetic.py
+++ skophys/datasets/_synthetic.py
@@ -4,6 +4,53 @@ from typing import Literal
 
 import numpy as np
 
+def eigen_decomposition(M):
+    # output eigenvalues and eigenvectors sorted by eigenvalues
+    
+    eigenvalues, eigenvectors = np.linalg.eig(M)
+    eigenvalues = np.real(eigenvalues)
+    eigenvectors = np.real(eigenvectors)
+    
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+    print (f"eigenvectors.shape = {eigenvectors.shape}")
+
+    return eigenvalues, eigenvectors
+
+def covariance_matrix(X):
+    # we assume X has a shape (N,T)
+    # N = number of features
+    # T = number of samples
+    
+    T = X.shape[1]
+    COV = X @ X.T / float(T)
+    
+    return COV
+
+def whitened_matrix(X, X0, k):
+    # we assume X has a shape (N,T)
+    # N = number of features
+    # T = number of samples
+
+    # COV shape = (N, N)
+    COV = covariance_matrix(X0)
+    S, U = eigen_decomposition(COV)
+
+    S_eco = S[:k]
+    U_eco = U[:, :k]
+
+    # Epsilon is added to the eigenvalues to prevent division by zero
+    epsilon = 1e-5
+    inv_sqrt_S = np.diag(1.0 / np.sqrt(S_eco + epsilon))
+
+    # whitening matrix W shape (N, N)
+    W = U_eco @ inv_sqrt_S @ U_eco.T
+
+    # whitened data shape (N, T)
+    Xw = W @ X
+
+    return Xw
 
 class ARProcess:
     def __init__(
@@ -14,6 +61,7 @@ class ARProcess:
         n_pixels_per_component: int = 1,
         obs_noise_sigma: float = 0.1,
         ar_noise_sigma: float = 0.1,
+        ar_bias: float = 0.0,
         n_background_pixels: int = 0,
         interpolation_factor: int = 1,
         random_seed: int = 0,
@@ -88,7 +136,7 @@ class ARProcess:
         n_rise = 0
 
         if spikes is None:
-            spikes = np.zeros((n_components, n_timepoints * interpolation_factor), dtype=bool)
+            spikes = np.zeros((n_components, (n_timepoints) * interpolation_factor), dtype=bool)
 
             np.random.seed(random_seed)
 
@@ -101,7 +149,10 @@ class ARProcess:
 
         if traces is None:
             for k_i in range(n_components):
+                ar_noise_sigma_k_i = np.random.uniform(low=0.0, high=ar_noise_sigma)
                 for t_i in range(2, n_timepoints):
+                    #gamma = np.array([1.5, -.55])
+                    #clean[k_i, t_i] = (gamma[0] * clean[k_i, t_i - 1]) + (gamma[1] * clean[k_i, t_i - 2])
                     if self.spikes[k_i, t_i]:
                         a = a_rise
                         a_decay = np.random.choice(a_options)
@@ -116,12 +167,17 @@ class ARProcess:
                     else:
                         a = a_decay
                     clean[k_i, t_i] = (a * clean[k_i, t_i - 1]) + (b * clean[k_i, t_i - 2]) + np.abs(
-                        np.random.normal(scale=ar_noise_sigma, size=1))
+                        np.random.normal(scale=ar_noise_sigma_k_i, size=1))
 
-                x_ = np.arange(0, n_timepoints * interpolation_factor)
-                xp_ = np.linspace(0, n_timepoints * interpolation_factor, n_timepoints)
+                x_ = np.arange(0, (n_timepoints) * interpolation_factor)
+                xp_ = np.linspace(0, (n_timepoints) * interpolation_factor, (n_timepoints))
                 clean[k_i] = np.interp(x_, xp_, fp=clean[k_i, :n_timepoints])
 
+            #clean = whitened_matrix(clean, clean, n_components)
+            for k_i in range(n_components):
+                bias = np.random.uniform(low=0.0, high=ar_bias)
+                for t_i in range(2, n_timepoints):
+                    clean[k_i, t_i] += bias
             self._traces = clean.copy()
 
             n_timepoints *= interpolation_factor
@@ -213,44 +269,67 @@ class ARProcessMovie(ARProcess):
             self,
             component_shape: Literal["rect", "gaussian"] = "gaussian",
             movie_dims: tuple[int, int] = (32, 32),
-            component_size: tuple[int, int] = (10, 10),
+            component_size: int = 2,
             component_locs: np.ndarray[int] | Literal["random"] = "random",
             component_locs_random_seed: int = 0,
             **kwargs,
     ):
         super().__init__(**kwargs)
 
-        r = -20, 20
-        r = np.linspace(*r)
+        r = -2*component_size, 2*component_size
+        r = np.linspace(*r, num=4*component_size+2)
+        print (f"r.shape = {r.shape}")
         x, y = np.meshgrid(r, r)
-        z = gaus2d(x, y)[20:-20, 20:-20]
+        z = gaus2d(x, y, sx=component_size, sy=component_size)
         z /= z.max()
+        print (f"z.shape = {z.shape}")
+
+        #r = -20, 20
+        #r = np.linspace(*r)
+        #x, y = np.meshgrid(r, r)
+        #z = gaus2d(x, y)[20:-20, 20:-20]
+        #z /= z.max()
+        #print (z.shape)
 
         if isinstance(component_locs, str):
             if component_locs == "random":
                 np.random.seed(component_locs_random_seed)
-                component_locs = (np.random.rand(kwargs["n_components"], 2) * movie_dims[0] - z.shape[0]).clip(0).astype(int)
+                #component_locs = (np.random.rand(kwargs["n_components"], 2) * movie_dims[0] - z.shape[0]).clip(0).astype(int)
+                component_locs = (np.random.rand(100, 2) * movie_dims[0]).clip(0).astype(int)
             else:
                 raise ValueError("invalid value for `component_locs`")
 
         k = self.params["n_components"]
 
         spatial_footprints = np.zeros((k, *movie_dims))
+        print (f"spatial_footprints.shape = {spatial_footprints.shape}")
 
-        for i in range(k):
-            cols = slice(component_locs[i, 0], component_locs[i, 0] + 10)
-            rows = slice(component_locs[i, 1], component_locs[i, 1] + 10)
-            spatial_footprints[i, rows, cols] = z
+        #for i in range(k):
+        #    cols = slice(component_locs[i, 0], component_locs[i, 0] + 10)
+        #    rows = slice(component_locs[i, 1], component_locs[i, 1] + 10)
+        #    spatial_footprints[i, rows, cols] = z
 
-        spatial_footprints = spatial_footprints.reshape(k, np.prod(movie_dims))
+        added_source = 0
+        for i in range(100):
+            if added_source==k: break
+            cols = slice(component_locs[i, 0] - int(z.shape[0]/2), component_locs[i, 0] + int(z.shape[1]/2))
+            rows = slice(component_locs[i, 1] - int(z.shape[0]/2), component_locs[i, 1] + int(z.shape[1]/2))
+            if z.shape == spatial_footprints[added_source, rows, cols].shape:
+                spatial_footprints[added_source, rows, cols] = z
+                added_source += 1
+        print (f"added_source = {added_source}")
+
+        serial_spatial_footprints = spatial_footprints.reshape(k, np.prod(movie_dims))
 
         X = self.traces
 
-        self._movie = (spatial_footprints.T @ X).reshape(*movie_dims, X.shape[1]).transpose(-1, 0, 1).copy()
+        self.kernel = z.copy()
+        self.source_footprints = spatial_footprints.copy()
+
+        self._movie = (serial_spatial_footprints.T @ X).reshape(*movie_dims, X.shape[1]).transpose(-1, 0, 1).copy()
 
         noise_sigma = kwargs["obs_noise_sigma"]
         noise = np.random.normal(scale=noise_sigma, size=np.prod(self._movie.shape)).reshape(self._movie.shape)
-
         self._movie += noise
 
     @classmethod
@@ -274,6 +353,18 @@ class ARProcessMovie(ARProcess):
 
 
     @property
+    #def source_footprints(self) -> np.ndarray:
+    #    """source_footprints, [k, rows, cols]"""
+    #    return self._source_footprints
+
+    #def traces(self) -> np.ndarray:
+    #    """traces, [rows, cols]"""
+    #    return self._traces
+
+    #def kernel(self) -> np.ndarray:
+    #    """kernel, [rows, cols]"""
+    #    return self._kernel
+
     def movie(self) -> np.ndarray:
         """movie, [t, rows, cols]"""
         return self._movie
